37ce256f557abdb7bbf4a41be7b33f162a5d6214
Billy <billydevbusiness@gmail.com>
2023-02-02 15:41:49 +0530

    Add Hinted Entry

--- example.py
+++ example.py
@@ -1,7 +1,11 @@
 import tkinter as tk
 from hintedtext import HintedText
+from hintedentry import HintedEntry
 
 root = tk.Tk()
 text = HintedText(root, hint="Enter your text here")
 text.pack()
+entry = HintedEntry(root, hint="Enter your text here")
+entry.pack()
+
 root.mainloop()
